73cb079124c9d4450ef15a0aebb96ca2daa21fea
vinodyadav_kannaboina <vinodyadavkannaboina@gmail.com>
2020-06-24 01:35:03 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -28,3 +28,26 @@ Tidydata <- merged_data %>% select(subject, code, contains("mean"), contains("st
 #Step 3: Uses descriptive activity names to name the activities in the data set.
 Tidydata$code <- activity_labels[TidyData$code, 2]
 
+#Step 4: Appropriately labels the data set with descriptive variable names.
+names(Tidydata)[2] = "activity"
+names(Tidydata)<-gsub("Acc", "Accelerometer", names(Tidydata))
+names(Tidydata)<-gsub("Gyro", "Gyroscope", names(Tidydata))
+names(Tidydata)<-gsub("BodyBody", "Body", names(Tidydata))
+names(Tidydata)<-gsub("Mag", "Magnitude", names(Tidydata))
+names(Tidydata)<-gsub("^t", "Time", names(Tidydata))
+names(Tidydata)<-gsub("^f", "Frequency", names(Tidydata))
+names(Tidydata)<-gsub("tBody", "TimeBody", names(Tidydata))
+names(Tidydata)<-gsub("-mean()", "Mean", names(Tidydata), ignore.case = TRUE)
+names(Tidydata)<-gsub("-std()", "STD", names(Tidydata), ignore.case = TRUE)
+names(Tidydata)<-gsub("-freq()", "Frequency", names(Tidydata), ignore.case = TRUE)
+names(Tidydata)<-gsub("angle", "Angle", names(Tidydata))
+names(Tidydata)<-gsub("gravity", "Gravity", names(Tidydata))
+
+#Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+FinalData <- Tidydata %>%
+    group_by(subject, activity) %>%
+    summarise_all(funs(mean))
+write.table(FinalData, "TidyData.txt", row.name=FALSE)
+
+
+
